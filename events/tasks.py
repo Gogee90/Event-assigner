9252d22ba00acb4d14e114bf9b39044b283b4c7c
Gogen <gogee09@gmail.com>
2022-01-19 23:18:19 +0500

    Fixed sending emails so it wouldn't send messages two or more times to the same user

--- events/tasks.py
+++ events/tasks.py
@@ -12,21 +12,27 @@ def create_reminder():
         today = datetime.now().strftime("%Y-%m-%d")
         event_date = (event.event_date - timedelta(days=1)).strftime("%Y-%m-%d")
         if event_date == today:
-            requests = Request.objects.filter(event=event)
-            for request in requests:
-                try:
-                    topic = "Напоминание"
-                    message = f"Напонминаем, что {event.event_date} стартует событие {event.name}"
-                    send_email(
-                        event.assigner.username,
-                        request.participant.email,
-                        topic,
-                        message,
-                    )
-                except Exception as e:
-                    send_email(
-                        event.assigner.username,
-                        event.assigner.email,
-                        "Ошибка",
-                        f"Произошла ошибка при отправке сообщений! {str(e)}",
-                    )
+            receivers = set(
+                [
+                    request.participant.email
+                    for request in Request.objects.filter(event=event)
+                ]
+            )
+            try:
+                topic = "Напоминание"
+                message = (
+                    f"Напонминаем, что {event.event_date} стартует событие {event.name}"
+                )
+                send_email(
+                    event.assigner.username,
+                    receivers,
+                    topic,
+                    message,
+                )
+            except Exception as e:
+                send_email(
+                    event.assigner.username,
+                    event.assigner.email,
+                    "Ошибка",
+                    f"Произошла ошибка при отправке сообщений! {str(e)}",
+                )
